a007a37c848a110ac2ecf6b86ab83aa301119a7a
osccso <osm428@hotmail.com>
2022-06-17 09:08:30 -0500

    api updated, previous one was down

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,5 @@
 import { colors,colorsPrimary } from "./data.js";
-const url = 'https://api.quotable.io/random';
+const url = 'https://zenquotes.io/api/random/0798b634182466f7e024f63744480f1aefc77180';
 let classTimeout
 
 export const getQuote = async () => {
@@ -10,16 +10,16 @@ export const getQuote = async () => {
   $("#author-quote").width();
   $("#text-quote").removeClass("fade-in")
   $("#author-quote").removeClass("fade-in")
-  const data = await response.json()
+  const [data] = await response.json()
   $("#text-quote").width();
   $("#author-quote").width();
   $("#text-quote").addClass("fade-in");
   $("#author-quote").addClass("fade-in");
-  $("#text-quote").text(data.content);
-  $("#author-quote").text(data.author);
+  $("#text-quote").text(data.q);
+  $("#author-quote").text(data.a);
   $(":root").css("--color", randomColor(colors));
   $(":root").css("--color2", randomColor(colorsPrimary));
-  $(".icon-twitter").attr("href",`https://twitter.com/intent/tweet?text=${data.content} - ${data.author}`);
+  $(".icon-twitter").attr("href",`https://twitter.com/intent/tweet?text=${data.q} - ${data.author}`);
   return data
 }
 
